a90fe8e9dda47701343f88688b82f9c750573e44
Michal Plewa <themipsial@gmail.com>
2022-07-29 03:00:21 +0200

    delete

--- models/test/departments.test.js
+++ models/test/departments.test.js
@@ -1,15 +1,11 @@
 const expect = require('chai').expect;
 const Department = require('../departments.model');
-const mongoose = require('mongoose');
 
 describe('Department', () => {
   it('should throw an error if no "name" is provided', () => {
     const department = new Department();
     department.validate((err) => {
       expect(err.errors.name).to.exist;
-    });
-    after(() => {
-      mongoose.models = {};
     });
   });
 });
