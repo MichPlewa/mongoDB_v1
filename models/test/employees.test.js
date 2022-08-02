4d5be7c8dc8d5c997c9e2e80853b9a1b32511da3
Michal Plewa <themipsial@gmail.com>
2022-08-02 20:22:47 +0200

    employees test

--- models/test/employees.test.js
+++ models/test/employees.test.js
@@ -8,4 +8,43 @@ describe('Employee', () => {
       expect(err.errors.firstName).to.exist;
     });
   });
+  it('should throw an error if "name" is not a string', () => {
+    const cases = [{}, []];
+    for (let employee of cases) {
+      const emp = new Employee({
+        firstName: employee,
+        lastName: employee,
+        department: employee,
+      });
+
+      emp.validate((err) => {
+        expect(err.errors.firstName).to.exist;
+      });
+    }
+  });
+  it('should throw an error if arguments does not contain firstName, lastName, department', () => {
+    const cases = [
+      { firstName: 'Anon' },
+      { lastName: 'Doe', department: 'Human' },
+      { firstName: 'Filip', lastName: 'Davidson' },
+    ];
+    for (let employee of cases) {
+      const emp = new Employee(employee);
+      emp.validate((err) => {
+        expect(err.errors).to.exist;
+      });
+    }
+  });
+  it('should not throw an error if firstName, lastName, department is correct', () => {
+    const cases = [
+      { firstName: 'Anon', lastName: 'Anon', department: 'Human' },
+      { firstName: 'Filip', lastName: 'Davidson', department: 'Human' },
+    ];
+    for (let employee of cases) {
+      const emp = new Employee(employee);
+      emp.validate((err) => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
 });
